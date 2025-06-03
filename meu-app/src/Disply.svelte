16de8388623cb918f89d16bf0b39608f4eea79dd
leticia <giovannaleticia02203@gmail.com>
2025-06-03 15:25:39 -0300

    ghk

---
+++ meu-app/src/Disply.svelte
@@ -0,0 +1,16 @@
+<script>
+  export let num;
+</script>
+
+<main>
+  <div>
+    <input type="text" class="input-text" value={num} />
+  </div>
+</main>
+
+<style>
+  input {
+    width: 245px;
+    height: 50px;
+  }
+</style>
--- script.js
+++ script.js
@@ -1,51 +1,50 @@
+let Clicou = false;
 
-  let Clicou = false
-  document.getElementById('atvButton').addEventListener('click', function () {
-    if (!Clicou) {
-        chama()
-        Clicou = true
-    } else {
-        fecha()
-        Clicou = false
-        document.getElementById("lista").innerHTML = "";
+// usuario clicou em atividades?
+document.getElementById("atvButton").addEventListener("click", function () {
+  if (!Clicou) {
+    chama();
+    Clicou = true;
+  } else {
+    fecha();
+    Clicou = false;
+    document.getElementById("lista").innerHTML = "";
+  }
 
-    }
+  var audio = document.getElementById("audioEntrada");
+  audio.play();
+});
 
-    var audio = document.getElementById('audioEntrada');
-    audio.play();
-  });
-  
+// exibir atividades
 function mostrarAtividades(baseURL, inicio, fim) {
-    const lista = document.getElementById("lista");
+  const lista = document.getElementById("lista");
 
-    for (let i = inicio; i <= fim; i++) {
-        const li = document.createElement("li");
-        const a = document.createElement("a");
+  for (let i = inicio; i <= fim; i++) {
+    const li = document.createElement("li");
+    const a = document.createElement("a");
 
-        a.href = `${baseURL}/${i}/index.html`;
-        a.target = "_blank";
-        a.textContent = `Atividade ${i}`;
-        a.classList.add("link-atividade");
+    a.href = `${baseURL}/${i}/index.html`;
+    a.target = "_blank";
+    a.textContent = `Atividade ${i}`;
+    a.classList.add("link-atividade");
 
-
-        li.appendChild(a);
-        lista.appendChild(li);
-    }
-    
+    li.appendChild(a);
+    lista.appendChild(li);
+  }
 }
 
-const chama = function() {
-    mostrarAtividades("https://leostrondaa.github.io/Atvs", 1, 9)
-    document.getElementById("colSecundaria").classList.remove("d-none");
-    document.getElementById("card").classList.remove("d-none");
-    document.getElementById("colPrimaria").classList.replace("col-12", "col-7");
-    document.getElementById("colSecundaria").classList.add("col-5");
-}
+// abrir pop-up
+const chama = function () {
+  mostrarAtividades("https://leostrondaa.github.io/Atvs", 1, 9);
+  document.getElementById("colSecundaria").classList.remove("d-none");
+  document.getElementById("card").classList.remove("d-none");
+  document.getElementById("colPrimaria").classList.replace("col-12", "col-7");
+  document.getElementById("colSecundaria").classList.add("col-5");
+};
 
-const fecha = function() {
-    document.getElementById("colSecundaria").classList.add("d-none");
-    document.getElementById("colPrimaria").classList.replace("col-7", "col-12");
-    document.getElementById("colSecundaria").classList.replace("col-5", "col-0");
-}
-
-
+// fechar pop-up
+const fecha = function () {
+  document.getElementById("colSecundaria").classList.add("d-none");
+  document.getElementById("colPrimaria").classList.replace("col-7", "col-12");
+  document.getElementById("colSecundaria").classList.replace("col-5", "col-0");
+};
