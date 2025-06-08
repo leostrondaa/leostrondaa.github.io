311d6ad8be1f96c7b1195997faa84f446288b591
leostrondaa <yaayleo@gmail.com>
2025-06-08 13:44:27 -0300

    uip

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
 let Clicou = false;
 
 // usuario clicou em atividades?
-document.getElementById("atvButton").addEventListener("click", function () {
+document.getElementById('atvButton').addEventListener('click', function () {
   if (!Clicou) {
     chama();
     Clicou = true;
   } else {
     fecha();
     Clicou = false;
-    document.getElementById("lista").innerHTML = "";
+    document.getElementById('lista').innerHTML = '';
   }
 
-  var audio = document.getElementById("audioEntrada");
+  var audio = document.getElementById('audioEntrada');
   audio.play();
 });
 
 // exibir atividades
 function mostrarAtividades(baseURL, inicio, fim) {
-  const lista = document.getElementById("lista");
+  const lista = document.getElementById('lista');
 
   for (let i = inicio; i <= fim; i++) {
-    const li = document.createElement("li");
-    const a = document.createElement("a");
+    const li = document.createElement('li');
+    const a = document.createElement('a');
 
     a.href = `${baseURL}/${i}/index.html`;
-    a.target = "_blank";
+    a.target = '_blank';
     a.textContent = `Atividade ${i}`;
-    a.classList.add("link-atividade");
+    a.classList.add('link-atividade');
 
     li.appendChild(a);
     lista.appendChild(li);
@@ -35,16 +35,16 @@ function mostrarAtividades(baseURL, inicio, fim) {
 
 // abrir pop-up
 const chama = function () {
-  mostrarAtividades("https://leostrondaa.github.io/Atvs", 1, 9);
-  document.getElementById("colSecundaria").classList.remove("d-none");
-  document.getElementById("card").classList.remove("d-none");
-  document.getElementById("colPrimaria").classList.replace("col-12", "col-7");
-  document.getElementById("colSecundaria").classList.add("col-5");
+  mostrarAtividades('https://leostrondaa.github.io/Atvs', 1, 10);
+  document.getElementById('colSecundaria').classList.remove('d-none');
+  document.getElementById('card').classList.remove('d-none');
+  document.getElementById('colPrimaria').classList.replace('col-12', 'col-7');
+  document.getElementById('colSecundaria').classList.add('col-5');
 };
 
 // fechar pop-up
 const fecha = function () {
-  document.getElementById("colSecundaria").classList.add("d-none");
-  document.getElementById("colPrimaria").classList.replace("col-7", "col-12");
-  document.getElementById("colSecundaria").classList.replace("col-5", "col-0");
+  document.getElementById('colSecundaria').classList.add('d-none');
+  document.getElementById('colPrimaria').classList.replace('col-7', 'col-12');
+  document.getElementById('colSecundaria').classList.replace('col-5', 'col-0');
 };
