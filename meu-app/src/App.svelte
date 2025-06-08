2e0231ae3dd0ef72c52400bea41ffe66b9ebbf73
leostrondaa <yaayleo@gmail.com>
2025-06-08 16:21:04 -0300

    mds funcionaaa

--- meu-app/src/App.svelte
+++ meu-app/src/App.svelte
@@ -1,30 +1,131 @@
 <script>
-	export let name;
+  import Display from './Display.svelte';
+  import Key from './Key.svelte';
+
+  let displayValue = '0';
+  let digitoAtual = '';
+  let digitoAnterior = '';
+  let operacao = null;
+  let resultado = null;
+
+  $: displayValue = digitoAtual || digitoAnterior || '0' || operacao;
+
+  function handleKeyPress(event) {
+    const key = event.detail;
+
+    if (key === 'C') {
+      clear();
+    } else if (key === '=') {
+      compute();
+    } else if (['+', '-', '*', '/'].includes(key)) {
+      Operacao(key);
+    } else {
+      if (
+        digitoAtual == '+' ||
+        digitoAtual == '-' ||
+        digitoAtual == '*' ||
+        digitoAtual == '/'
+      ) {
+        digitoAtual = '';
+      }
+      digitoAtual += key;
+    }
+  }
+
+  function Operacao(op) {
+    if (digitoAtual === '') {
+      return;
+    }
+    if (digitoAnterior !== '') {
+      compute();
+    }
+
+    operacao = op;
+    digitoAnterior = digitoAtual;
+    digitoAtual = op;
+  }
+
+  function clear() {
+    digitoAtual = '';
+    digitoAnterior = '';
+    operacao = null;
+    resultado = null;
+  }
+
+  function compute() {
+    const anterior = parseFloat(digitoAnterior);
+    const atual = parseFloat(digitoAtual);
+
+    switch (operacao) {
+      case '+':
+        resultado = anterior + atual;
+        break;
+      case '-':
+        resultado = anterior - atual;
+        break;
+      case '*':
+        resultado = anterior * atual;
+        break;
+      case '/':
+        resultado = anterior / atual;
+        break;
+      default:
+        return;
+    }
+
+    digitoAtual = resultado.toString();
+    operacao = null;
+    digitoAnterior = '';
+  }
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+<main class="cor">
+  <div class="container">
+    <div class="card">
+      <Display value={displayValue} readonly />
+
+      <Key on:keypress={handleKeyPress} />
+    </div>
+  </div>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  .container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    min-height: 100vh;
+    width: 100%;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  .card {
+    width: 25%;
+    height: 75vh;
+    background-color: white;
+    border-radius: 10px;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    padding: 20px;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  .input-text {
+    width: 65%;
+    padding: 10px;
+    margin-bottom: 20px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    text-align: right;
+    font-size: 1.5em;
+  }
+
+  .cor {
+    background-color: burlywood;
+  }
+  input {
+    width: 245px;
+    height: 50px;
+  }
 </style>
---
+++ meu-app/src/Key.svelte
@@ -0,0 +1,75 @@
+<script>
+  import { createEventDispatcher } from 'svelte';
+  const dispatch = createEventDispatcher();
+
+  function Digito(value) {
+    dispatch('keypress', value);
+  }
+</script>
+
+<div class="button-row">
+  <button class="btn" on:click={() => Digito('1')}>1</button>
+  <button class="btn" on:click={() => Digito('2')}>2</button>
+  <button class="btn" on:click={() => Digito('3')}>3</button>
+  <button class="btn operador" on:click={() => Digito('+')}>+</button>
+</div>
+<div class="button-row">
+  <button class="btn" on:click={() => Digito('4')}>4</button>
+  <button class="btn" on:click={() => Digito('5')}>5</button>
+  <button class="btn" on:click={() => Digito('6')}>6</button>
+  <button class="btn operador" on:click={() => Digito('-')}>-</button>
+</div>
+<div class="button-row">
+  <button class="btn" on:click={() => Digito('7')}>7</button>
+  <button class="btn" on:click={() => Digito('8')}>8</button>
+  <button class="btn" on:click={() => Digito('9')}>9</button>
+  <button class="btn operador" on:click={() => Digito('*')}>*</button>
+</div>
+<div class="button-row">
+  <button class="btn" on:click={() => Digito('0')}>0</button>
+  <button class="btn operador" on:click={() => Digito('/')}>/</button>
+  <button class="btn operador" on:click={() => Digito('=')}>=</button>
+  <button class="btn C" on:click={() => Digito('C')}>C</button>
+</div>
+
+<style>
+  .button-row {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 10px;
+    width: 70%;
+  }
+
+  .btn {
+    background-color: black;
+    color: white;
+    border: none;
+    border-radius: 6px;
+    padding: 15px 20px;
+    margin: 0 5px;
+    cursor: pointer;
+    font-size: 1.2em;
+    flex: 1;
+    transition: background-color 0.3s;
+  }
+
+  .btn:hover {
+    background-color: #333;
+  }
+
+  .operador {
+    background-color: #ff9500;
+  }
+
+  .operador:hover {
+    background-color: #e68a00;
+  }
+
+  .C {
+    background-color: #ff3b30;
+  }
+
+  .C:hover {
+    background-color: #e63329;
+  }
+</style>
